c39bbd4e6c95a93b601316b04f4fa976b838f140
Rob Patro <rob@newton>
2018-08-04 22:13:42 -0400

    IT WORKS; can allocate a CQF from rust

--- cqf/src/lib.rs
+++ cqf/src/lib.rs
@@ -3,3 +3,21 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use std::ptr;
+  #[test]
+  fn basic_cqf() {
+	    let mut qf:QF = QF{ runtimedata: ptr::null_mut(),
+                          metadata: ptr::null_mut(),
+                          blocks: ptr::null_mut() };
+      let nslots = 256;
+      let nhashbits = 8;
+      unsafe {
+          let r =  qf_malloc(&mut qf, nslots, nhashbits, 0, qf_hashmode::QF_HASH_INVERTIBLE, 0);
+      }
+	//qf_malloc(&qf, nslots, nhashbits, 0, QF_HASH_INVERTIBLE, 0)) {
+  }
+}
